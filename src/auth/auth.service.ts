84a1020e7d808237adb99f1cc8a8c0c6ce93ef39
andrey-kupets <andrey.kupets@i.ua>
2022-10-05 16:17:34 +0300

    add @unique, add relation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import * as argon from 'argon2';
+
 import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup(dto: AuthDto) {
-    return { msg: "i've signed up"};
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      // how to display selected fields
+      // select: {
+      //   id: true,
+      //   email: true,
+      //   createdAt: true
+      // },
+    });
+
+    delete user.hash;
+
+    return user;
   }
 
   signin() {
