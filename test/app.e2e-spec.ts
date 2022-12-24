8f56b12378a532f00a1159fe41aa2f8b0b677a6f
andrey-kupets <andrey.kupets@i.ua>
2022-12-24 20:31:27 +0200

    get me test w/o commits

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -52,31 +52,31 @@ describe('App e2e', () => {
           .expectStatus(400)
       });
 
-      // it('should throw if password empty', () => {
-      //   return pactum
-      //     .spec()
-      //     .post('/auth/signup')
-      //     .withBody({
-      //       email: dto.email,
-      //     })
-      //     .expectStatus(400)
-      // });
-      //
-      // it('should throw if no body provided', () => {
-      //   return pactum
-      //     .spec()
-      //     .post('/auth/signup')
-      //     .expectStatus(400)
-      // });
-      //
-      // it('should signup', () => {
-      //   return pactum
-      //     .spec()
-      //     .post('/auth/signup')
-      //     .withBody(dto)
-      //     .expectStatus(201)
-      //     // .inspect();
-      // });
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400)
+      });
+
+      it('should throw if no body provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .expectStatus(400)
+      });
+
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201)
+          // .inspect();
+      });
     });
 
     describe('Signin', () => {
